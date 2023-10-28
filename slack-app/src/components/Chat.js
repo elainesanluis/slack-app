0027329a9c603f940e58f2cd23a05127b76da0b5
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-10-28 13:16:10 +0800

    fixed bug related to firebase, undefined value, added ChatInput styles and code

--- slack-app/src/components/Chat.js
+++ slack-app/src/components/Chat.js
@@ -1,13 +1,43 @@
+import { InfoOutlined, StarBorderOutlined } from '@mui/icons-material';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { selectRoomId } from '../features/appSlice';
+import ChatInput from './ChatInput';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
+
 
 function Chat () {
+    const roomId = useSelector(selectRoomId);
+    const [roomDetails] = useDocument(
+        roomId && db.collection('rooms').doc(roomId)
+    );
+    const [roomMessages] = useCollection(
+        roomId && 
+        db
+        .collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+    );
+    console.log(roomDetails?.data());
+    console.log(roomMessages);
+
     return <ChatContainer>
-        <h1>Hello</h1>
-
         <Header>
-
+        <HeaderLeft>
+            <h4><strong>#Room-name</strong></h4>
+        </HeaderLeft>
+        <StarBorderOutlined/>
+        <HeaderRight>
+            <p>
+                <InfoOutlined/>Details
+            </p>
+        </HeaderRight>
         </Header>
+        <ChatMessages></ChatMessages>
+        <ChatInput channelName={roomDetails?.data().name} channelId={roomId}></ChatInput>
     </ChatContainer>;
 }
 
@@ -20,4 +50,42 @@ const ChatContainer = styled.div`
  margin-top: 60px;
 `;
 
-const Header = styled.div``;
+const Header = styled.div`
+display:flex;
+justify-content: space-between;
+padding: 20px;
+border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+display: flex;
+align-items: center;
+
+>h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+}
+
+>h4 > .MuiSvgIcon-root {
+    margin-left: 10px;
+    font-size: 18px;
+}
+`;
+
+
+const HeaderRight = styled.div`
+>p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+}
+
+>p >.MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+}
+`;
+
+const ChatMessages = styled.div``;
+
