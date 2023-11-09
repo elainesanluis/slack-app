b76e34504d2497066c95e20d03e020f456da7f49
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-11-09 20:57:55 +0800

    added login to App.js

--- slack-app/src/App.js
+++ slack-app/src/App.js
@@ -5,11 +5,19 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import styled from "styled-components";
 import Chat from './components/Chat';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
+import Login from './Login';
 
 function App() {
+  const [user, loading] = useAuthState(auth);
   return (
     <div className="App">
       <Router>
+       {!user? (
+          <Login/>
+        ): (
+          <>
         <Header/>
           <Appbody>
             <Sidebar/>
@@ -17,6 +25,9 @@ function App() {
              <Route path='/' exact element={<Chat></Chat>}></Route>
           </Routes>
         </Appbody>
+        </>
+        )
+      };
       </Router>
     </div>
   );
