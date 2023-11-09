e5fac82512542f3eee8f1387733177f23a275fd3
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-11-09 20:45:35 +0800

    added code to login page

--- slack-app/src/Login.js
+++ slack-app/src/Login.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <LoginContainer>
+        <LoginInnerContainer>
+            <img src='https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg' alt=''/>
+            <h1>Sign in</h1>
+            <p>avion.slack.com</p>
+        </LoginInnerContainer>
+    </LoginContainer>
   )
 }
 
 export default Login;
+
+const LoginContainer = styled.div``;
+
+const LoginInnerContainer = styled.div``;
