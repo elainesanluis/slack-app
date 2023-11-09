34bcd95f56028befcda5f64ee2b8195cfb69b416
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-11-09 22:24:08 +0800

    added button in login page to sign in

--- slack-app/src/components/Login.js
+++ slack-app/src/components/Login.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import styled from "styled-components";
+import styled from 'styled-components';
+import { Button } from '@mui/material';
 
 function Login() {
+  const signIn = (e) => {
+  e.preventDefault();
+  };
   return (
     <LoginContainer>
         <LoginInnerContainer>
             <img src='https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg' alt=''/>
             <h1>Sign in</h1>
             <p>avion.slack.com</p>
+
+        <Button onClick={signIn}>
+          Sign in with Google
+        </Button>
         </LoginInnerContainer>
     </LoginContainer>
-  )
+  );
 }
 
 export default Login;
@@ -27,11 +35,20 @@ padding: 100px;
 text-align: center;
 background-color: white;
 border-radius: 10px;
+box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 
 >img {
   object-fit: contain;
   height: 100px;
   margin-bottom: 40px;
 }
+
+>button {
+  margin-top: 50px;
+  text-transform: inherit !important;
+  background-color: #0a8d48 !important;
+  color: white;
+}
+
 `;
 
