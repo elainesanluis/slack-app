ed2f739bd9b1c05d6a4cbf2e8f1e0c949963cab7
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-11-10 23:58:35 +0800

    added chat to dashboard

--- slack-app/src/components/Dashboard.js
+++ slack-app/src/components/Dashboard.js
@@ -2,14 +2,15 @@ import React, { useState, useContext } from 'react'
 import styled from 'styled-components';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function Dashboard() {
   return (
     <div>
-
      <Header/>
-       <Appbody>
-         <Sidebar/>
+      <Appbody>
+       <Sidebar/>
+       <Chat/>
      </Appbody>
    </div>
   )
