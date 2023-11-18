b583092ff93f72902c58f973987ac327ae92a147
ELAINE SAN_LUIS <ladyelaine.sanluis@gmail.com>
2023-11-18 23:49:29 +0800

    updated UI of login

--- slack-app/src/pages/Login.js
+++ slack-app/src/pages/Login.js
@@ -1,8 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Link, Navigate } from 'react-router-dom';
-import { FormContainer, FormLayout, Errors } from '../components/styles';
+import { Errors } from '../components/styles';
 import { UserContext } from '../contexts/User';
 import usePost from '../hooks/usePost';
+import styled from 'styled-components';
 
 export default function Login() {
 	const [userInfo, setUserInfo] = useState({
@@ -44,8 +45,9 @@ export default function Login() {
 	}
 
 	return (
-		<FormLayout>
-			<FormContainer>
+		<LoginContainer>
+        <LoginInnerContainer>
+		<img src='https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg' alt=''/>
 				<h1>Sign In to Workspace</h1>
 				<p>
 					Enter your <span>email</span> and <span>password</span>
@@ -77,7 +79,31 @@ export default function Login() {
 					<button type="submit">Sign In</button>
 				</form>
 				<Link to="/signup">Create an account</Link>
-			</FormContainer>
-		</FormLayout>
+        </LoginInnerContainer>
+		</LoginContainer>
 	);
 }
+
+const LoginContainer = styled.div`
+background-color: #f8f8f8;
+height: 100vh;
+display: flex;
+flex-direction: column;
+place-items: center;
+`;
+
+const LoginInnerContainer = styled.div`
+padding: 100px;
+text-align: center;
+background-color: white;
+border-radius: 10px;
+box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+
+>img {
+  object-fit: contain;
+  height: 100px;
+  margin-bottom: 40px;
+}
+`;
+
+
